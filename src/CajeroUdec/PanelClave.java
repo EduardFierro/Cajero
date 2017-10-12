cbb6e267d34d6df63890d7f96b921a38fd2360ac
Unknown <rrssangie@gmail.com>
2017-10-11 21:22:46 -0500

    PanelClave

---
+++ src/CajeroUdec/PanelClave.java
@@ -0,0 +1,90 @@
+package CajeroUdec;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class PanelClave extends JPanel{
+	public PanelClave() {
+		this.setBackground(Color.white);
+		setLayout(new GridBagLayout());
+		GridBagConstraints constraint = new GridBagConstraints();
+		constraint.gridx = 0;
+		constraint.gridy = 0;
+		constraint.fill = GridBagConstraints.BOTH;
+		Insets insets = new Insets(40, 50, 40, 50);
+		constraint.insets = insets;
+		
+		JLabel titulo = new JLabel("Cambio de clave");
+		titulo.setForeground(Color.blue);
+		constraint = new GridBagConstraints();
+		constraint.gridx = 0; 
+		constraint.gridy = 0;
+		constraint.fill = GridBagConstraints.BOTH; 
+		insets = new Insets(8, 10, 8, 10);
+		constraint.insets = insets;
+		add(titulo, constraint);
+		
+		JLabel spacewhite = new JLabel("                                                 ");
+		titulo.setForeground(Color.blue);
+		constraint = new GridBagConstraints();
+		constraint.gridx = 1; 
+		constraint.gridy = 1;
+		constraint.fill = GridBagConstraints.BOTH; 
+		insets = new Insets(8, 10, 8, 10);
+		constraint.insets = insets;
+		add(spacewhite, constraint);
+		
+		JLabel conActual = new JLabel("Contraseña actual: ");
+		constraint = new GridBagConstraints();
+		constraint.gridx = 0; 
+		constraint.gridy = 1;
+		constraint.fill = GridBagConstraints.BOTH; 
+		insets = new Insets(8, 10, 8, 10);
+		constraint.insets = insets;
+		add(conActual, constraint);
+		
+		JPasswordField conAct = new JPasswordField();
+		constraint = new GridBagConstraints();
+		constraint.gridx = 1; 
+		constraint.gridy = 1;
+		constraint.fill = GridBagConstraints.BOTH; 
+		insets = new Insets(8, 10, 8, 10);
+		constraint.insets = insets;
+		add(conAct, constraint);
+		
+		JLabel conNueva = new JLabel("Contraseña nueva: ");
+		constraint = new GridBagConstraints();
+		constraint.gridx = 0; 
+		constraint.gridy = 2;
+		constraint.fill = GridBagConstraints.BOTH; 
+		insets = new Insets(8, 10, 8, 10);
+		constraint.insets = insets;
+		add(conNueva, constraint);
+		
+		JPasswordField conNue = new JPasswordField();
+		constraint = new GridBagConstraints();
+		constraint.gridx = 1; 
+		constraint.gridy = 2;
+		constraint.fill = GridBagConstraints.BOTH; 
+		insets = new Insets(8, 10, 8, 10);
+		constraint.insets = insets;
+		add(conNue, constraint);
+		
+		JButton continuar = new JButton("CONTINUAR");
+		constraint = new GridBagConstraints();
+		constraint.gridx = 0; 
+		constraint.gridy = 3;
+		constraint.fill = GridBagConstraints.BOTH; 
+		insets = new Insets(8, 10, 8, 10);
+		constraint.insets = insets;
+		add(continuar, constraint);
+	}
+}
